f72c2519d1b24f780431faf3fcfff87dd1d269da
Ankitsingh2820 <105094969+Ankitsingh2820@users.noreply.github.com>
2025-07-26 19:55:31 +0530

    Time: 1 ms (91.22%), Space: 45.1 MB (53.51%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -13,44 +13,34 @@
  *     }
  * }
  */
-import java.util.*;
-
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-    TreeNode() {}
-    TreeNode(int val) { this.val = val; }
-    TreeNode(int val, TreeNode left, TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
-    }
-}
-
 class Solution {
-    public List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> result = new ArrayList<>();
-        if (root == null) return result;
+    public List<List<Integer>> levelOrder(TreeNode root) 
+    {
+        Queue<TreeNode> que= new LinkedList<>();
+        List<List<Integer>> blist=new ArrayList<>();
+        if(root==null) return blist;
+        que.offer(root);
+        while(!que.isEmpty())
+        {
+            int level=que.size();
+            List<Integer> slist=new ArrayList<>();
+            for(int i=0;i<level;i++)
+            {
+                if(que.peek().left!=null)
+                {
+                    que.offer(que.peek().left);
 
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.offer(root);
+                }
+                if(que.peek().right!=null)
+                {
+                    que.offer(que.peek().right);
+                }
+                slist.add(que.poll().val);
 
-        while (!queue.isEmpty()) {
-            int levelSize = queue.size();
-            List<Integer> level = new ArrayList<>();
 
-            for (int i = 0; i < levelSize; i++) {
-                TreeNode current = queue.poll();
-                level.add(current.val);
-
-                if (current.left != null) queue.offer(current.left);
-                if (current.right != null) queue.offer(current.right);
             }
-
-            result.add(level);
+            blist.add(slist);
         }
-
-        return result;
+        return blist;
     }
 }
