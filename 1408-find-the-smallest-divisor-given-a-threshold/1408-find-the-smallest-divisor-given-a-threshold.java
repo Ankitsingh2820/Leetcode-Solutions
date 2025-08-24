0edc6b8eb62aff4baa7069338e87637103b943b9
Ankitsingh2820 <105094969+Ankitsingh2820@users.noreply.github.com>
2025-08-24 13:57:05 +0530

    Time: 7 ms (84.88%), Space: 48.1 MB (81.01%) - LeetHub

--- 1408-find-the-smallest-divisor-given-a-threshold/1408-find-the-smallest-divisor-given-a-threshold.java
+++ 1408-find-the-smallest-divisor-given-a-threshold/1408-find-the-smallest-divisor-given-a-threshold.java
@@ -1,31 +1,39 @@
 class Solution {
-    public int smallestDivisor(int[] nums, int threshold) {
-        int left = 1, right = 0;
-        for (int num : nums) {
-            right = Math.max(right, num);  // maximum possible divisor
+    public int smallestDivisor(int[] nums, int threshold) 
+    {
+        int low=1;
+        int high=0;
+
+        for(int num:nums)
+        {
+             high = Math.max(high, num);
         }
-
-        int ans = right;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-
-            if (canDivide(nums, threshold, mid)) {
-                ans = mid;         // possible answer
-                right = mid - 1;   // try smaller divisor
-            } else {
-                left = mid + 1;    // need bigger divisor
+        int ans=high;
+        while(low<=high)
+        {
+            int mid=low+(high-low)/2;
+            if(can( nums, threshold,mid))
+            {
+                ans=mid;
+                high=mid-1;
+            }
+            else
+            {
+                low=mid+1;
             }
         }
         return ans;
+        
     }
+    private boolean can(int[] nums, int threshold,int k)
+    {
+        int count=0;
+        for(int num:nums)
+        {
+            count+=(num+k-1)/k;
+            if(count>threshold) return false;
 
-    // Check if divisor d works
-    private boolean canDivide(int[] nums, int threshold, int d) {
-        int sum = 0;
-        for (int num : nums) {
-            sum += (num + d - 1) / d;  // ceil(num/d)
-            if (sum > threshold) return false; // early exit
         }
-        return sum <= threshold;
+        return true;
     }
 }
